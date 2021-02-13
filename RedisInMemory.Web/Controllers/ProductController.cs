faca66d0361a93c679963d645610db34cb575f92
salih <salihkoc3469@gmail.com>
2021-02-13 16:49:03 +0300

    AddMemoryCache() servis ve IMemoryCache interface

---
+++ RedisInMemory.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisInMemory.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private  IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
