49a756ddd7d04bb6dc53a1e75e5ca2f1803943d2
salih <salihkoc3469@gmail.com>
2021-02-13 18:31:06 +0300

    Asp.Net Core + In-Memory : RegisterPostEvictionCallback Method

--- RedisInMemory.Web/Controllers/ProductController.cs
+++ RedisInMemory.Web/Controllers/ProductController.cs
@@ -22,10 +22,15 @@ namespace RedisInMemory.Web.Controllers
                 MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
 
                
-                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1); //--> AbsoluteExpiration
+                options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); //--> AbsoluteExpiration
                 options.SlidingExpiration = TimeSpan.FromSeconds(10); //--> Sliding Expiration
 
                 options.Priority = CacheItemPriority.High;
+
+                options.RegisterPostEvictionCallback((key, value, reason, state) =>
+                {
+                    _memoryCache.Set("callback", $"{key}->{value}=>sebep:{reason}");
+                });
 
                 _memoryCache.Set<string>("tarih", DateTime.Now.ToString(), options);
             }
@@ -35,8 +40,9 @@ namespace RedisInMemory.Web.Controllers
         public IActionResult Show()
         {
             _memoryCache.TryGetValue("tarih", out string tarihcache);
-
+            _memoryCache.TryGetValue("callback", out string callback);
             ViewBag.zaman = tarihcache;
+            ViewBag.callback = callback;
 
             return View();
         }
