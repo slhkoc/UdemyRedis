405884a8f85b0fe531c57e27ede2e1c9fc56b160
salih <salihkoc3469@gmail.com>
2021-02-21 15:08:56 +0300

    Asp.Net Core + StackExchange.Redis API - StackExchange.Redis API / Redis String

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+        public IActionResult Index()
+        {
+            db.StringSet("name", "salih");
+            db.StringSet("ziyaretci", 100);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //var value = db.StringGet("name");
+            //var value = db.StringGetRange("name",0,3);
+            var value = db.StringLength("name");
+
+            //db.StringIncrement("ziyaretci", 1);
+
+            //var count=db.StringDecrementAsync("ziyaretci", 1).Result;
+
+            db.StringDecrementAsync("ziyaretci", 10).Wait();
+
+
+            ViewBag.value = value.ToString();
+
+
+            return View();
+        }
+    }
+}
