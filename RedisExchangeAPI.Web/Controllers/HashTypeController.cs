da3833472593f5bdad687677d7fb155f4ef99c8e
salih <salihkoc3469@gmail.com>
2021-02-21 16:53:01 +0300

    Asp.Net Core + StackExchange.Redis API - StackExchange.Redis API / Redis Hash

---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        public string hashkey { get; set; } = "sozluk";
+        public HashTypeController(RedisService redisService): base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+            if(db.KeyExists(hashkey))
+            {
+                db.HashGetAll(hashkey).ToList().ForEach(x =>
+                {
+                    list.Add(x.Name, x.Value);
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name,string val)
+        {
+            db.HashSet(hashkey, name, val);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.HashDelete(hashkey, name);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
